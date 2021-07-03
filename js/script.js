b28a23a1608f4ce9ccde2706c06f5e5922e00edd
tomohatsu <tomohatsu-design@outlook.com>
2021-07-03 10:47:01 +0900

    slick fix

--- js/script.js
+++ js/script.js
@@ -1,40 +1,43 @@
-$(function(){
 
-// スティッキーヘッダー
-//*スクロールすると色が付く
+//sticky header
 
-  var $win = $(window),
-      $top = $('.top'),
-      $header = $('.header'),
-      topHeight = $top.outerHeight();
-      fixedClass = 'fixed';
-    
-    $win.on('load scroll',function(){
-      var value = $(this).scrollTop();
-      if($win.width() > 768){
-        if(value > topHeight){
+  $(function() {
+    var $win = $(window),
+        $fv = $('.fv'),
+        $header = $('.header')
+        fvHeight = $fv.outerHeight(),
+        fixedClass = 'fixed';
+  
+  $win.on('load scroll',function(){
+    var value = $(this).scrollTop();
+      if($win.width()>768)
+  
+        if ( value > fvHeight) {
           $header.addClass(fixedClass);
-        }else{
+          
+        } else {
           $header.removeClass(fixedClass);
+          
         }
-      }
+      
+  
     });
-});
+  });
 
-// ハンバーガーメニュー
-$('.hamburger-btn').click(function(){
-  $(this).toggleClass('active');
-  $('.header__nav').fadeToggle(300);
-  $('body').toggleClass('noscroll');
-});
 
-// スライダー
-// slickを使用
-$('.slider').slick({
-  autoplay: true,
-  autoplaySpeed: 2000,
-  speed: 1000,
-  fade: true,
-  cssEase: 'linear'
-});
+  //ファーストビューのslider
+  $('.slider').slick({
+    autoplay: true,
+    autoplaySpeed: 2000,
+    speed: 1000,
+    fade: true,
+    cssEase: 'linear'
+  });
 
+  
+  //ハンバーガーメニューの開閉
+  $('.burger-btn').on('click',function(){
+    $('.header-nav').fadeToggle(300);
+    $('.burger-btn').toggleClass('cross');
+    $('body').toggleClass('noscroll');
+  });
